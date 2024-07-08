95f84cbec042325fe2942f143516f9d815a92c3b
ronaldfb <ronaldferrazb@gmail.com>
2024-07-08 15:26:34 -0300

    ...

---
+++ aula16/funcao01.js
@@ -0,0 +1,12 @@
+function parimpar(n) {
+    if (n%2==0){
+        return 'Par'
+    }else{
+        return 'Ímpar'
+    }
+}
+
+console.log(parimpar(11))
+
+// let res = parimpar(4)
+// console.log(res)
