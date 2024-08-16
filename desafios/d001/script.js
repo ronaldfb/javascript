bad86467d6e8b536c109bfb51ec22959ce915b68
ronaldfb <ronaldferrazb@gmail.com>
2024-08-16 14:59:16 -0300

    ..

---
+++ desafios/d001/script.js
@@ -0,0 +1,11 @@
+function botao1() {
+    window.alert("Você clicou no primeiro botão!")
+}
+
+function botao2() {
+    window.alert("Você clicou no segundo botão!")
+}
+
+function botao3() {
+    window.alert("Você clicou no terceiro botão!")
+}
