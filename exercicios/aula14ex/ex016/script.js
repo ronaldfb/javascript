82c43ec92f342dbb1da902b6d402aafd04d083cc
ronaldfb <ronaldferrazb@gmail.com>
2024-09-19 18:06:51 -0300

    .

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -1,35 +1,38 @@
 function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
-    
+    let ini = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+    let res = document.getElementById('resultado')
 
-    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossível contar!'
-        window.alert('[ERRO] Faltam dados!')
-    }else{
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'Preencha todos os campos!'
+
+
+    }else {
         res.innerHTML = 'Contando: <br>'
+
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+
         if(p <= 0) {
-            window.alert('Passo inválido! Considerando PASSO 1')
+            window.alert('Impossível contar! Considerando PASSO 1')
             p = 1
         }
-
-        if (i < f){
-            //Contagem crescente
+        if(i < f){
+            //Crescente
             for(let c = i; c <= f; c += p){
-                res.innerHTML +=  `${c} \u{1F449}`
-            }
-        }else {
-            //Contagem regressiva
-            for (let c = i; c >= f; c -= p) {
                 res.innerHTML += `${c} \u{1F449}`
-
             }
-        } 
-        res.innerHTML += `\u{1F3C1}`
+        }else{
+            //Regressiva
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+
+        res.innerHTML += `🏴‍☠️`
     }
+
+
 }
