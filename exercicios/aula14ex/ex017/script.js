0f34d2ffa1c8d384f104d29361dea008cfe56da1
ronaldfb <ronaldferrazb@gmail.com>
2024-09-20 13:27:11 -0300

    .

--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,19 +1,29 @@
 function tabuada() {
-    let num = document.getElementById('txtn')
+    let num = document.getElementById('num')
     let tab = document.getElementById('seltab')
-    if (num.value.length == 0) {
-        window.alert('Por favor, digite um número!')
+
+    if(num.value.length == 0) {
+        window.alert('Por favor, Digite um número!!')
     }else {
         let n = Number(num.value)
-        let c = 1
+
         tab.innerHTML = ''
-        while(c <= 10) {
+
+        for(let c = 1; c <= 10; c++) {
             let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
-            item.value = `tab${c}`
+            item.text += `${n} x ${c} = ${n*c}`
+            item.value += `tab${c}`
             tab.appendChild(item)
-            c++
+
         }
+        // c = 1
+        // while(c<=10){
+        //     let item = document.createElement('option')
+        //     item.text = `${n} x ${c} = ${n*c}`
+        //     item.value = `tab${c}`
+        //     tab.appendChild(item)
+        //     c++
+        // }
     }
-    
+
 }
