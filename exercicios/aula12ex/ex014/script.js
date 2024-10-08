94e00fe8bc70b397e93c03811780df124ebf86be
ronaldfb <ronaldferrazb@gmail.com>
2024-10-08 14:50:15 -0300

    Update script.js

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -3,22 +3,22 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    //var hora = 18
+    // var hora = 18
     msg.innerHTML = `Agora são ${hora} horas.`
     
     if (hora >= 0 && hora < 12) {
         //BOM DIA!
-        img.src = 'morning-250.png'
+        img.src = 'imgs/morning-250.png'
         document.body.style.background = '#d5d8d1'
         msg2.innerHTML = 'Bom dia!'
     }else if (hora >=12 && hora < 18){
         //BOA TARDE!
-        img.src = 'tarde250.png'
+        img.src = 'imgs/tarde250.png'
         document.body.style.background = '#fcc86a'
         msg2.innerHTML = 'Boa Tarde!'
     }else{
         //BOA NOITE!
-        img.src = 'noite250.png'
+        img.src = 'imgs/noite250.png'
         document.body.style.background = '#005f93'
         msg2.innerHTML = 'Boa Noite!'
     }
